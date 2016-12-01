095070edf0cfedbdfd326e7b94a30899c780ef3d
George Daou <georgehdaou@yahoo.com>
2016-12-01 12:57:43 -0500

    Controller improvement

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function contact()
+    {
+        return 'This is the contact page';
+    }
+    public function help()
+    {
+        return 'This is the help page';
+    }
+    public function faq()
+    {
+        return 'This is the faq page';
+    }
+    public function testimonials()
+    {
+        return 'This is the testimonials page';
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,31 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'BusServiceController@index')->name('busservice.index');
+
+Route::get('/charters', 'CharterController@index')->name('charters.index');
+Route::get('/charters/create', 'CharterController@create')->name('charters.create');
+Route::post('/charters', 'CharterController@store')->name('charters.store');
+Route::get('/charters/{charter}', 'CharterController@show')->name('charters.show');
+Route::get('/charters/{charter}/edit', 'CharterController@edit')->name('charters.edit');
+Route::put('/charters/{charter}', 'CharterController@update')->name('charters.update');
+Route::delete('/charters/{charter}', 'CharterController@destroy')->name('charters.destroy');
+
+Route::get('/excursions', 'ExcursionController@index')->name('excursion.index');
+Route::get('/excursions/create', 'ExcursionController@create')->name('excursions.create');
+Route::post('/excursions', 'ExcursionController@store')->name('excursions.store');
+Route::get('/excursions/{excursion}', 'ExcursionController@show')->name('excursions.show');
+Route::get('/excursions/{excursion}/edit', 'ExcursionController@edit')->name('excursions.edit');
+Route::put('/excursions/{excursion}', 'ExcursionController@update')->name('excursions.update');
+Route::delete('/excursions/{excursion}', 'ExcursionController@destroy')->name('excursions.destroy');
+
+Route::get('/contact', 'PageController@contact')->name('page.contact');
+
+Route::get('/help', 'PageController@help')->name('page.help');
+
+Route::get('/faq', 'PageController@faq')->name('page.faq');
+
+Route::get('/testimonials', 'PageController@testimonials')->name('page.testimonials');
