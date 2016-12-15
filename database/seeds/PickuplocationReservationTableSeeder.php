d6b0ccfc889e8daa202ab92509b2a9fdbdc0bf96
George Daou <georgehdaou@yahoo.com>
2016-12-15 14:38:19 +0200

    final commit

--- database/seeds/PickuplocationReservationTableSeeder.php
+++ database/seeds/PickuplocationReservationTableSeeder.php
@@ -17,15 +17,15 @@ class PickuplocationReservationTableSeeder extends Seeder
          # First, create an array of all the books we want to associate tags with
          # The *key* will be the book title, and the *value* will be an array of tags.
          $reservations =[
-             '1' => ['North_Shore_Mall','South_Shore_Mall'],
+             '1' => ['North_Shore_Mall','South_Shore_ Mall'],
              '2' => ['North_Shore_Mall','Boston_Common'],
          ];
 
          # Now loop through the above array, creating a new pivot for each book to tag
-         foreach($reservations as $event_id => $pickuplocations) {
+         foreach($reservations as $res_id => $pickuplocations) {
 
              # First get the reservation
-             $reservation = Reservation::where('event_id','like',$event_id)->first();
+             $reservation = Reservation::where('id','like',$res_id)->first();
 
              # Now loop through each tag for this book, adding the pivot
              foreach($pickuplocations as $pickuplocationName) {
